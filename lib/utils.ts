cd8875fe171c76e6c4a2f8a49dded555cc323d50
mizokuiam <mrmizoku@gmail.com>
2024-12-01 02:40:31 +0100

    accordion.tsx

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,10 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
+  /**
+   * Combines multiple class names using clsx and handles Tailwind class conflicts using tailwind-merge
+   * @param inputs - Class names to be combined
+   * @returns Merged class string
+   */
   return twMerge(clsx(inputs));
 }
